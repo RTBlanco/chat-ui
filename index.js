fcc6361e5ca54b3a681d06da8747b55348f4ef28
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-06 07:03:44 -0400

    added interactive bot

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () =>{
   const formInput = document.getElementById('input-form');
-  const userInput = []
+  let userInput
   
   formInput.addEventListener('submit', (e) => {
     e.preventDefault();
-    let formText = e.target[0].value
-    userInput.push(formText)
-    createToMessage(formText)
-    // console.log(userInput)
+    let userInput = e.target[0].value
+    
+    createToMessage(userInput)
+    setTimeout(()=> {
+      createFromMessge(userInput)
+    },1000)
+    formInput.reset()
   })
 
   
@@ -18,6 +21,41 @@ function createToMessage(text) {
   let content = `
     <div class="to-message">
       ${text}
+    </div>
+  `
+  messageArea.innerHTML += content
+}
+
+function createFromMessge(text) {
+  const messageArea = document.getElementById('messages');
+  let response
+  switch (text) {
+    case "hey":
+      response = "Hey how are you!"
+      break;
+    
+    case "what are you":
+      response = "I am a simple Bot in this simple world"
+      break;
+
+    case "your are so cool":
+      response = "I know!!! Thanks to Ronny! someone should really hire this man"
+      break;
+
+    case "doing good":
+      response = "Well you probably doing better then Ronny he's going crazy looking for a job"
+      break;
+
+    case "bye":
+      response = "By Remeber to follow ronny on Twitter @R_T_Blanco!!"
+
+    default:
+      response = "Im sorry Im not a smart bot.... yet can you repeat that please?"
+      break;
+  }
+  let content = `
+    <div class="from-message">
+      ${response}
     </div>
   `
   messageArea.innerHTML += content
